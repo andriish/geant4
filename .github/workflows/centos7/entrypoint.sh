fcbe9ff1efa53bac9d5e5eedb862cda62e21dcd7
Alois Kabelschacht <kabel@Aloiss-Air.fritz.box>
2021-01-18 20:19:47 +0100

    Added ci

---
+++ .github/workflows/centos7/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -l
+set -x
+uname -a 
+cat /etc/issue
+yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum -y install  gcc gcc-c++ gcc-gfortran make which
+yum -y install  cmake3 cmake3-data
+yum -y install  HepMC3*
+#yum -y install  lapack-static  lapack-devel lapack  gengetopt  blas-devel blas atlas-devel atlas
+#yum -y install  libX11-devel libX11  libXmu-devel libXmu libXau-devel libXau libXcursor-devel  libXcursor  libSM-devel libSM libICE libICE-devel libXext-devel libXext
+#yum -y install  root-*6* --exclude=*doc* --exclude=*debug* --skip-broken
+
+#sh jadeinstall.sh
+
+
+
+#yum -y install pythia8-devel pythia8 pythia8-data 
+
+
+#sh jadetest.sh
+
+out=$?
+echo ::set-output name=out::$out
---
+++ .github/workflows/osx/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -l
+set -x
+brew install wget coreutils gcc
+brew tap davidchall/hep
+brew install hepmc3
+brew cask install xquartz
+#wget --no-verbose https://root.cern/download/root_v6.22.02.macosx64-10.15-clang110.tar.gz
+#tar -zxf root_v6.22.02.macosx64-10.15-clang110.tar.gz
+#. root/bin/thisroot.sh
+
+#sh jadeinstall.sh
+
+
+
+#brew install pythia8
+
+#sh jadetest.sh
