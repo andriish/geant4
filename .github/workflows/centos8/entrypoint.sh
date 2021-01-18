c8a3cdecd210ee2796460015d59410d7a9ae45cb
Alois Kabelschacht <kabel@Aloiss-Air.fritz.box>
2021-01-18 21:17:52 +0100

    Better ci

--- .github/workflows/centos8/entrypoint.sh
+++ .github/workflows/centos8/entrypoint.sh
@@ -7,7 +7,8 @@ yum -y install dnf-plugins-core
 dnf config-manager --set-enabled PowerTools
 dnf config-manager --set-enabled powertools
 yum -y install  gcc gcc-c++ gcc-gfortran make which cmake cmake-data cmake-filesystem
-yum -y install  HepMC3 HepMC3-devel HepMC HepMC-devel
+yum -y install  HepMC3 HepMC3-devel 
+#HepMC HepMC-devel
 yum -y install yum-plugin-copr
 yum -y copr enable averbyts/HEPrpms
 yum -y install geant4 geant4-devel clhep clhep-devel PTL-devel redhat-rpm-config expat expat-devel zlib zlib-devel
