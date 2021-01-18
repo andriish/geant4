ad48c2142d25d909b2f37a7905079c58cfeadd9d
Alois Kabelschacht <kabel@Aloiss-Air.fritz.box>
2021-01-18 20:31:57 +0100

    Better ci

--- .github/workflows/centos8/entrypoint.sh
+++ .github/workflows/centos8/entrypoint.sh
@@ -6,22 +6,19 @@ yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarc
 yum -y install dnf-plugins-core
 dnf config-manager --set-enabled PowerTools
 dnf config-manager --set-enabled powertools
-yum -y install  gcc gcc-c++ gcc-gfortran make which
-yum -y install  cmake cmake-data cmake-filesystem
-yum -y install  HepMC3*
+yum -y install  gcc gcc-c++ gcc-gfortran make which cmake cmake-data cmake-filesystem
+yum -y install  HepMC3 HepMC3-devel
 yum -y install yum-plugin-copr
 yum -y copr enable averbyts/HEPrpms
 yum -y install geant4
-#yum -y install  lapack-static  lapack-devel lapack  gengetopt  blas-devel blas atlas-devel atlas  openblas-devel openblas openblas-serial64 openblas-threads  --skip-broken
-#yum -y install  libX11-devel libX11  libXmu-devel libXmu libXau-devel libXau libXcursor-devel  libXcursor  libSM-devel libSM libICE libICE-devel libXext-devel libXext
-#yum -y install  root-*6* --exclude=*doc* --exclude=*debug* --skip-broken
-
-#sh jadeinstall.sh
-
-
 #yum -y install pythia8-devel pythia8 pythia8-data 
 
-#sh jadetest.sh
+
+mkdir -p test
+cd test
+cmake -B. -S ../examples/extended/eventgenerator
+make -j 2
+
 
 out=$?
 echo ::set-output name=out::$out
